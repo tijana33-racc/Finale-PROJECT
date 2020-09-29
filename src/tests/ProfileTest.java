873d907b13745efcd47d081ee492015187411406
tijana33-racc <tijana9paunovic@gmail.com>
2020-09-29 22:03:20 +0200

    WIP ProfileTest

---
+++ src/tests/ProfileTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.AuthPage;
+import pages.LocationPopupPage;
+import pages.LoginPage;
+import pages.NotificationSystemPage;
+import pages.ProfilePage;
+
+public class ProfileTest extends BasicTest {
+	LocationPopupPage lpp = new LocationPopupPage(driver, wait);
+	LoginPage lp = new LoginPage(driver, wait);
+	NotificationSystemPage nsp = new NotificationSystemPage(driver, wait);
+	AuthPage ap = new AuthPage(driver, wait);
+
+
+	@Test(priority = 0)
+	public void testEditProfile() throws InterruptedException {
+		driver.navigate().to(baseUrl + "/guest-user/login-form");
+		lpp.closePopUp();
+		lp.LogIn(this.email, this.password);
+		lp.getLoginBtn().click();
+		String msg = "Login Successfull";
+		Assert.assertEquals(lpp.getPopUp().getText(), msg, "[ERROR] login is not succeful!");
+		Thread.sleep(3000);
+		driver.navigate().to(baseUrl + "/member/profile");
+		lp.getEmail().clear();
+		lp.getPassword().clear();
+		lp.LogIn("tijana.paunica@gmail.com", this.password);
+		Thread.sleep(3000);
+
+		lp.getLoginBtn().click();
+		String smsg = "Setup Successful";
+		Assert.assertEquals(nsp.getMsg(), smsg, "[ERROR] setup failed");
+		Thread.sleep(3000);
+
+		ap.getLogout();
+		String logout = "Logout Successfull!";
+		Assert.assertEquals(nsp.AlertMsg(), logout, "[ERROR] logout failed!");
+
+	}
+
+	@Test(priority = 5)
+	public void testChangeProfile() {
+		this.driver.navigate().to(baseUrl + "/guest-user/login-form");
+		lpp.closePopUp();
+		lp.LogIn(this.email, this.password);
+		String login= "Login Successfull"; 
+		Assert.assertEquals(nsp.AlertMsg(), login, "[ERROR] login failed");
+
+
+
+	}
+
+}
