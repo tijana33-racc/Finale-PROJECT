412841d17af3813d45e41bb207c16c7cddb1aba6
tijana33-racc <tijana9paunovic@gmail.com>
2020-09-29 18:48:08 +0200

    AuthPage is added to scr

---
+++ src/pages/AuthPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage{
+
+	public AuthPage(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+	}
+public WebElement getAccount() {
+	return this.driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/ul/li[1]"));
+}
+public WebElement getMyAccount() {
+	return this.driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/ul/li/div/ul/li[1]"));
+}
+public WebElement getLogout() {
+	return this.driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/ul/li/div/ul/li[2]"));
+}
+
+}
