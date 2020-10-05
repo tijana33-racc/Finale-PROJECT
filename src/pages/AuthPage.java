5a0e37d58aeebfe622ad5b5b879cf6708259d89a
tijana33-racc <tijana9paunovic@gmail.com>
2020-10-05 20:55:49 +0200

    New logOut method hase been added to AuthPage

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -19,5 +19,7 @@ public WebElement getMyAccount() {
 public WebElement getLogout() {
 	return this.driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/ul/li/div/ul/li[2]"));
 }
-
+public void logOut() { 
+	this.getLogout().click();
+}
 }
